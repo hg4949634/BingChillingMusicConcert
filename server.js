0cdefc6751a9d88b330bb49efea06042dcc38287
EmptyS1ot <hg4949634@gmail.com>
2025-04-10 02:48:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from "path";
 const __dirname = path.resolve();
 const app = express();
 app.use(express.static("frontend"));
-const port = 3000;
+const port = 80;
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/frontend/Welcome.html");
 });
